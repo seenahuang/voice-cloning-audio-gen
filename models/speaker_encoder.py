6a34de6cb76af7bfeb8b663b20c708b24f40fc9e
Seena Huang <seena.huang@gmail.com>
2022-11-20 16:52:51 -0800

    speaker encoder linear layer

--- models/speaker_encoder.py
+++ models/speaker_encoder.py
@@ -2,10 +2,12 @@ import torch
 
 
 class SpeakerEncoder(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers=3):
+    def __init__(self, input_size, hidden_size, device, num_layers=3, embedding_size=256):
         super(SpeakerEncoder, self).__init__()
-        self.LSTM = torch.nn.LSTM(input_size, hidden_size, num_layers)
-        #TODO: linear layer
+        self.LSTM = torch.nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.linear = torch.nn.Linear(hidden_size, embedding_size)
+
+        self.device = device
 
     def forward(self, data):
         # TODO: get LSTM output, add in final linear layer to 256 dimensions, perform L2 normalization
