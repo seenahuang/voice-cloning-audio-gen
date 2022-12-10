4960c7bca46292ab22e9bb52b58fbec649ee5481
tsyoshihara <=>
2022-12-10 14:47:24 -0800

    main file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import utils
 import torchaudio
+from models.speaker_encoder import SpeakerEncoder
+import torch
+from models.denoiser import Denoiser
+import IPython.display as ipd
 
 if __name__ == "__main__":
     test_audio_gen = utils.load_test()
@@ -13,3 +17,42 @@ if __name__ == "__main__":
     # 4. Call generate() method of tacotron with processed text + speaker embedding
     # 5. Take resulting spectrogram and pass through waveglow
     # 6. Save "speaker_0_after.wav" from waveglow
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    params = utils.retrieve_hyperparams("speaker_encoder.yaml")
+    sampling_rate = 22050
+
+    text = "sample text to generate"
+
+    #initialize speaker encoder
+    speaker_encoder_checkpoint_path = "checkpoints/speaker_encoder.pth"
+    speaker_encoder = SpeakerEncoder(params['input_size'],
+                             params['hidden_size'],
+                             device,
+                             num_layers=params['num_layers'],
+                             embedding_size=params['embedding_size'])
+    speaker_encoder.load_state_dict(torch.load(speaker_encoder_checkpoint_path))
+    speaker_encoder.eval()
+
+    embeddings = None
+
+    #initialize tacotron
+    tacotron_path = "checkpoints/tacotron2.pt"
+    tacotron = None
+
+
+    #initialize waveglow
+    waveglow_path = 'checkpoints/waveglow.pt'
+    waveglow = torch.load(waveglow_path)['model']
+    waveglow.cuda().eval().half()
+    for k in waveglow.convinv:
+        k.float()
+    denoiser = Denoiser(waveglow)
+
+    mel_outputs, linear, attn_scores = tacotron.generate(text, embeddings)
+
+    with torch.no_grad():
+        audio = waveglow.infer(linear, sigma=0.666)
+    ipd.Audio(audio[0].data.cpu().numpy(), rate=sampling_rate)
+
+    audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+    ipd.Audio(audio_denoised.cpu().numpy(), rate=sampling_rate)
