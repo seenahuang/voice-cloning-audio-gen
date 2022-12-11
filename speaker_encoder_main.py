8af404b047ddf9da51aad4194da0debb263cef85
Seena Huang <seena.huang@gmail.com>
2022-12-10 22:20:21 -0800

    using ray tune

--- speaker_encoder_main.py
+++ speaker_encoder_main.py
@@ -7,6 +7,10 @@ import os
 import sys
 import copy
 import json
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+from functools import partial
 
 REMOVED_SPEAKERS_TRAINING = {1992, 8014, 7312, 445, 1183} # speaker IDs that have fewer than 80 utterances for training
 
@@ -51,14 +55,26 @@ def validate(epoch, data_loader, model, criterion):
     return torch.mean(losses)
 
 
-if __name__ == "__main__":
-    params = utils.retrieve_hyperparams("speaker_encoder.yaml")
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def train_encoder(config, params=None, device=torch.device('cpu'), checkpoint_dir=None):
+    # initialize/load model
+    encoder = SpeakerEncoder(params['input_size'],
+                             params['hidden_size'],
+                             device,
+                             num_layers=params['num_layers'],
+                             embedding_size=params['embedding_size'])
 
+    optimizer = torch.optim.SGD(encoder.parameters(), config['learning_rate'])
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=config['decay_step'], gamma=config['decay_gamma'])
+    criterion = EndToEndLoss(10.0, -5.0, device)
+
+    if checkpoint_dir:
+        model_state, optimizer_state = torch.load(os.path.join(checkpoint_dir, "checkpoint"))
+        encoder.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
+
+    # load data
     train_librispeech = utils.load_train()
     valid_librispeech = utils.load_validation()
-    test_librispeech = utils.load_test()
-
 
     if os.path.isfile('./data/processed/train.pt'):
         train_data = torch.load('./data/processed/train.pt')
@@ -83,63 +99,93 @@ if __name__ == "__main__":
     train_spec_size = (params['train_spec_length'], params['spec_channels'])
     validate_spec_size = (params['validate_spec_length'], params['spec_channels'])
     train_loader = torch.utils.data.DataLoader(dataset=train_data,
-                                               batch_size=params['batch_size'],
+                                               batch_size=config['batch_size'],
                                                shuffle=True,
                                                collate_fn=lambda x: utils.generate_spectrograms(x,
                                                                                                 device,
                                                                                                 train_spec_size))
 
     validation_loader = torch.utils.data.DataLoader(dataset=validation_data,
-                                               batch_size=params['batch_size'],
+                                               batch_size=config['batch_size'],
                                                shuffle=True,
                                                collate_fn=lambda x: utils.generate_spectrograms(x,
                                                                                                 device,
                                                                                                 validate_spec_size))
 
-    encoder = SpeakerEncoder(params['input_size'],
-                             params['hidden_size'],
-                             device,
-                             num_layers=params['num_layers'],
-                             embedding_size=params['embedding_size'])
-    optimizer = torch.optim.SGD(encoder.parameters(), params['learning_rate'])
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=params['decay_step'], gamma=params['decay_gamma'])
-    criterion = EndToEndLoss(10.0, -5.0, device)
-
-    with open('speaker_encoder_loss.json', 'r') as f:
-        loss_dict = json.load(f)
-        overall_best_loss = loss_dict['best_loss']
-
-    best_model = None
-    best_loss = sys.maxsize
-
-    train_losses = []
-    val_losses = []
-
     for epoch in range(params['epochs']):
-        curr_train_loss = train(epoch, train_loader, encoder, optimizer, criterion)
-        train_losses.append(curr_train_loss.item())
+        running_loss = 0.0
+        epoch_steps = 0
+        for i, spectrograms in enumerate(train_loader):
+            if torch.cuda.is_available():
+                spectrograms = spectrograms.cuda()
 
-        curr_val_loss = validate(epoch, validation_loader, encoder, criterion)
-        val_losses.append(curr_val_loss.item())
+            optimizer.zero_grad()
 
-        if curr_val_loss < best_loss:
-            best_loss = curr_val_loss
-            best_model = copy.deepcopy(encoder)
+            out = encoder.forward(spectrograms)
+            loss = criterion(out)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            epoch_steps += 1
+            print(f"[{epoch+1}, {i+1}] loss: {running_loss/epoch_steps}")
+
+        val_loss = 0.0
+        val_steps = 0
+        for i, spectrograms in enumerate(validation_loader):
+            if torch.cuda.is_available():
+                spectrograms = spectrograms.cuda()
+
+            with torch.no_grad():
+                out = encoder.forward(spectrograms)
+                loss = criterion(out)
+                val_loss += loss.cpu().numpy()
+                val_steps += 1
+
+        with tune.checkpoint_dir(epoch) as checkpoint_dir:
+            path = os.path.join(checkpoint_dir, "checkpoint")
+            torch.save((encoder.state_dict(), optimizer.state_dict()), path)
 
         scheduler.step()
+        tune.report(loss=(val_loss / val_steps))
+    print("Finished Training")
 
-    print(f'Best Loss: {best_loss}\n')
 
-    if best_loss < overall_best_loss:
-        torch.save(best_model.state_dict(), 'checkpoints/speaker_encoder/speaker_encoder.pth')
+def main(num_samples=10, max_num_epochs=50):
+    params = utils.retrieve_hyperparams("speaker_encoder.yaml")
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        loss_dict['best_loss'] = int(best_loss)
-        with open('speaker_encoder_loss.json', "r+") as f:
-            f.seek(0)
-            f.write(json.dumps(loss_dict))
-            f.truncate()
+    config = {
+        "batch_size": tune.choice([4,8,16]),
+        "learning_rate": tune.loguniform(1e-2, 2e-1),
+        "decay_gamma": tune.loguniform(0.1, 1),
+        "decay_step": tune.choice([2, 5, 10])
+    }
 
-        utils.plot_curves(range(params['epochs']), train_losses, val_losses)
-        print('Found better model')
-    else:
-        print('Did not find better model')
+    scheduler = ASHAScheduler(
+        metric="loss",
+        mode="min",
+        max_t=max_num_epochs,
+        grace_period=1,
+        reduction_factor=2)
+    reporter = CLIReporter(metric_columns=["loss"])
+
+    result = tune.run(
+        partial(train_encoder, params=params, device=device),
+        resources_per_trial={"cpu": 12, "gpu":1},
+        config=config,
+        num_samples=num_samples,
+        scheduler=scheduler,
+        progress_reporter=reporter
+    )
+
+    best_trial = result.get_best_trial("loss")
+    print(f"Best trial config: {best_trial.config}")
+    print(f"Best trial loss: {best_trial.last_result['loss']}")
+
+    best_checkpoint_dir = best_trial.checkpoint.value
+    model_state, _ = torch.load(os.path.join(best_checkpoint_dir, "checkpoint"))
+    torch.save(model_state, 'checkpoints/best_speaker_encoder.pth')
+
+if __name__ == "__main__":
+    main()
